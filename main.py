87ff6dfd7455981bef7e7e361aeb5fc52ec0b867
308 <308@academicos.cesde.edu.co>
2022-02-17 07:27:08 -0500

    se crea hola mundo con python

---
+++ main.py
@@ -0,0 +1,33 @@
+#Esto es un comentario de linea
+
+'''
+Comentario de bloque
+
+'''
+
+#SALIDA CONSOLA
+#print("HOLA MUNDO PYTHON OME")
+
+#VARIABLES O ENTRADAS
+nombreUsuario:str='Juan'
+edad:int=32
+estatura:float=1.62
+eshinchaVerde:bool=True
+
+#salida
+#print(nombreUsuario)
+#print('Buenos dias',nombreUsuario)
+#print(f'Buenos dias {nombreUsuario}')
+
+#RECIBIENDO VARIABLES DESDE LA CONSOLA
+#numero1=int(input("Digita un número entero: "))
+#print(f'usted digito el numero {numero1}')
+
+#numero2=int(input("Digita un segundo número entero: "))
+#print(f'usted digito el numero {numero2}')
+
+#suma=numero1+numero2
+#print(f'la suma de {numero1} + {numero2} = {suma}')
+
+numero:int="juan"
+print(numero)
