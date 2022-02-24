6f5645f43cd206181a517ecec4eb5c2fa1063f0d
308 <308@academicos.cesde.edu.co>
2022-02-24 07:41:05 -0500

    se agregan rutians if y while introductorias a python

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ Comentario de bloque
 #print("HOLA MUNDO PYTHON OME")
 
 #VARIABLES O ENTRADAS
-nombreUsuario:str='Juan'
-edad:int=32
-estatura:float=1.62
-eshinchaVerde:bool=True
+#nombreUsuario:str='Juan'
+#edad:int=32
+#estatura:float=1.62
+#eshinchaVerde:bool=True
 
 #salida
 #print(nombreUsuario)
@@ -29,5 +29,52 @@ eshinchaVerde:bool=True
 #suma=numero1+numero2
 #print(f'la suma de {numero1} + {numero2} = {suma}')
 
-numero:int="juan"
-print(numero)
+#numero:int="juan"
+#print(numero)
+
+
+#TOMANDO DECISIONES
+#numero = int (input("Ingrese un numero : "))
+#print(f'El numero digitado fue {numero}')
+
+#if(numero>=0):
+    #print("el numero es positivo")
+#else:
+    #print("el numero es negativo")
+    
+    #print("oe.. fuck you men")
+
+#Ejemplo 2
+'''
+nivelAgua=int(input("digite el nivel de agua: "))
+
+if(nivelAgua<200):
+    print("hay muy poca agua en la represa")
+elif(nivelAgua>=200 and nivelAgua<450):
+    print("el nivel de agua el el adecuado")
+else:
+    print("hay mucha agua, ojo...")
+'''
+'''
+mes = input("digite el mes: ")
+
+if( mes == "enero" or mes =="febrero" or mes =="marzo"):
+    print(f"estamos en invierno")
+elif( mes == "abril" or mes =="mayo" or mes=="junio"):
+    print(f"estamos en verano")
+elif( mes == "julio" or mes == "agosto" or mes == "septiembre"):
+    print(f"estamos en otoño")
+else:
+    print(f"estamos en primavera")
+'''
+
+#bucles/ciclos/loops/repeticion
+
+contador=0
+
+while(contador<10):
+    contador+=1
+    print(contador)
+else:
+    print("termino el ciclo")
+    
